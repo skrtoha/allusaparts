a5d942c36127689dc42beffc02cc74d56c669fb9
skrtoha <root@server.concord-parts.com>
2022-06-29 17:34:12 +0000

    Development

--- views/layouts/main_ru.php
+++ views/layouts/main_ru.php
@@ -49,7 +49,21 @@ $mainMenu = AllusapartsMenu::getMenu();
     <?php $this->registerCsrfMetaTags() ?>
 
     <?php $this->head() ?>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
+   ym(89339147, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/89339147" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 </head>
 <body class="page-1">
 <?php $this->beginBody() ?>
